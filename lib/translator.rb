c7a272b28b035b8241c443f77bf2757594a3f4a3
Kelly Rudnicki <kelly.rudnicki@comcast.net>
2019-11-13 17:39:15 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,10 +15,10 @@ def load_library(file)
 end
 
 def get_japanese_emoticon(file, emoticon)
-  # code goes here
+  # code goes here, takes traditional western emoticon and translate it to japnese version, rely on load_library
 end
 
 def get_english_meaning(file, emoticon)
-  # code goes here
+  # code goes here, takes japanese emoticon and returns meaning in english, rely on load_library
   pp file
 end
